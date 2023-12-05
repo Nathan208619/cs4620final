8e44132a190630173944abe0165a11ab8c8d443c
Nathan208619 <nf208619@ohio.edu>
2023-12-05 13:31:05 -0500

    webpage cleaned up, cleaning up repo now

--- webpage/app.py
+++ webpage/app.py
@@ -16,6 +16,27 @@ def query_the_database(conn, query):
         print(e)
 
 # ---------ARTIST VISUALIZATIONS---------
+def plot_artist_songs_chart(artist):
+
+    # query the database
+    conn = sqlite3.connect("music.db")
+    query = "SELECT title, total_streams FROM most_streams WHERE artist=" + "'" + artist + "'"
+    data = query_the_database(conn, query)
+    conn.close()
+    if data is None:
+        print(f"No data found for the artist: {artist}")
+        return
+    titles, daily_streams = zip(*data)
+
+    # query the database
+    plt.subplots(figsize=(25, 25))
+    plt.pie(daily_streams, autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired.colors, textprops={'fontsize': 21})
+    plt.legend(titles, title='Track Legend', bbox_to_anchor=(1, 0.5), loc="center left", fontsize='large', title_fontsize='large')
+
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.savefig('./static/visualizations/artist_songs.png')
+
 def plot_artist_album_chart(artist):
     # Query the albums_streams table
     conn = sqlite3.connect("music.db")
@@ -33,33 +54,12 @@ def plot_artist_album_chart(artist):
     plt.xlabel('Titles', fontsize=18)
     plt.ylabel('Album Streams', fontsize=18)
     title = f"{artist}'s most streamed albums among the top 200"
-    plt.title(title, fontsize=20)
-    
-    # Rotate x-axis labels for better readability
-    plt.xticks(rotation=45, ha='right', fontsize=12)  # Adjust the rotation angle as needed
+    plt.title(title, fontsize=20)    
+    plt.xticks(rotation=30, ha='right', fontsize=18)
+    plt.yticks(fontsize=18)
     plt.tight_layout()
     plt.savefig('./static/visualizations/artist_albums.png')
 
-def plot_artist_songs_chart(artist):
-
-    # query the database
-    conn = sqlite3.connect("music.db")
-    query = "SELECT title, total_streams FROM most_streams WHERE artist=" + "'" + artist + "'"
-    data = query_the_database(conn, query)
-    conn.close()
-    if data is None:
-        print(f"No data found for the artist: {artist}")
-        return
-    titles, daily_streams = zip(*data)
-
-    # query the database
-    plt.subplots(figsize=(25, 25))
-    plt.pie(daily_streams, autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired.colors, textprops={'fontsize': 18})
-    plt.legend(titles, title='Track Legend', bbox_to_anchor=(1, 0.5), loc="center", fontsize='large', title_fontsize='xx-large')
-
-    plt.axis('equal')
-    plt.tight_layout()
-    plt.savefig('./static/visualizations/artist_songs.png')
 
 def build_artist_top_songs_by_daily_streams(artist):
     conn = sqlite3.connect("music.db")
@@ -71,7 +71,7 @@ def build_artist_top_songs_by_daily_streams(artist):
         return
     titles, daily_streams = zip(*data)
 
-    plt.subplots(figsize=(10, 8))
+    plt.subplots(figsize=(15, 10))
     plt.pie(daily_streams, autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired.colors, textprops={'fontsize': 18})
     plt.legend(titles, title='Track Legend', bbox_to_anchor=(1, 0.5), loc="center", fontsize='small', title_fontsize='medium')
 
@@ -102,7 +102,11 @@ def main():
 @app.route('/artists', methods=['GET', 'POST'])
 def artist_page():
     if request.method == 'POST':
-        artist_name = request.form['artist_name']
+        artist_name = request.form['artist_name'].lower()   
+        names = artist_name.split(" ")
+        names = [name.capitalize() for name in names]
+        artist_name = " ".join(names)
+        print(artist_name)
         plot_artist_album_chart(artist_name)
         plot_artist_songs_chart(artist_name)
         build_artist_top_songs_by_daily_streams(artist_name)
@@ -111,32 +115,15 @@ def artist_page():
 
 @app.route('/visualizations/<artist_name>', methods=['GET'])
 def visualizations(artist_name):
-    # Perform data processing and visualization here based on the artist_name
-    # For example, you can call a function that generates a chart
-    # chart = artists_most_streamed_songs_bar_chart(artist_name)
-
-    # Pass the artist_name and chart to the template
     return render_template('artists_visualizations.html', artist_name=artist_name)
 
-@app.route('/songs', methods=['GET', 'POST'])
+@app.route('/songs', methods=['GET'])
 def song_page():
-    if request.method == 'POST':
-        song_name = request.form['song_name']
-        # Perform visualization based on song_name using execute_query function
-        # Example: result = execute_query("SELECT * FROM songs WHERE title=?", (song_name,))
-        # Add your visualization logic here
-        return render_template('songs', song_name=song_name)
     return render_template('songs.html')
 
-@app.route('/albums', methods=['GET', 'POST'])
+@app.route('/albums', methods=['GET'])
 def album_page():
-    if request.method == 'POST':
-        album_name = request.form['album_name']
-        # Perform visualization based on album_name using execute_query function
-        # Example: result = execute_query("SELECT * FROM albums WHERE title=?", (album_name,))
-        # Add your visualization logic here
-        return render_template('albums', album_name=album_name)
-    return render_template('albums.html')
+    return render_template('artists-comparisons.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
